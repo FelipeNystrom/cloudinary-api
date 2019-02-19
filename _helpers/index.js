b6a394500afb407a41025b1fac26f41a1078818b
FelipeNystrom <andre.felipe.nystrom@gmail.com>
2019-02-19 22:33:16 +0100

    add put and delete route, update and post is moved to own function

--- _helpers/index.js
+++ _helpers/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const { uploadImg } = require('../_image-and-video-storage');
 const pathToTempDir = path.resolve(__dirname, '../', '_tmp-media-storage');
 
 /*  helper function to clear temporary image storage on server */
@@ -34,7 +35,82 @@ const corsOptions = {
   }
 };
 
+uploadOrUpdateImg = async (req, res) => {
+  if (!req.file) {
+    return res.status(400).send({
+      Error_message:
+        'No file is sent in request! You have to send a image appended to a form to this route.'
+    });
+  }
+
+  const {
+    body: { folderName, description = null },
+    file: { path, originalname },
+    method
+  } = req;
+
+  //  Check if file is included in request
+  if (!path) {
+    return res.status(400).send({
+      Error_message:
+        'No image found in request! You have to send a image appended to a form to this route.'
+    });
+  }
+
+  //   Send back error when image is not sent right
+  if (!folderName) {
+    return res.status(400).send({
+      Error_message:
+        'Missing destination folder in form, matching key "folderName"'
+    });
+  }
+  // Check description type to be of type string
+  if (description !== null && typeof description !== 'string') {
+    return res.status(400).send({
+      Error_message: 'image description must be of type string'
+    });
+  }
+
+  try {
+    /*
+    Extract img form req object through req.file path and send to 
+    Cloudinary upload function. Second parameter is foldername on Cloudinary 
+    */
+
+    const cloudinaryResult = await uploadImg(path, folderName);
+
+    /* 
+    Result form upload is an object where public id and https link is saved and sent back in response
+    */
+
+    const { public_id, secure_url } = cloudinaryResult;
+
+    let action;
+
+    if (method === 'PUT') {
+      action = 'updated old image with ';
+    }
+
+    if (method === 'POST') {
+      action = 'saved';
+    }
+
+    res.send({
+      message: `Succefully ${action} image: "${originalname}" on cloudinary in folder ${folderName}`,
+      'img-link': secure_url,
+      'public-id': public_id
+    });
+  } catch (err) {
+    /* If error send error response with catched error as feedback */
+    console.error(`Error: ${err}`);
+    res.status(500).send({ Error_message: `A error was catched: ${err}` });
+  }
+
+  tempStorageTruncate();
+};
+
 module.exports = {
   tempStorageTruncate,
-  corsOptions
+  corsOptions,
+  uploadOrUpdateImg
 };
--- _routes/handleUploads.js
+++ _routes/handleUploads.js
@@ -1,61 +1,38 @@
 const Router = require('express-promise-router');
 const router = new Router();
-const pHash = require('phash-imagemagick');
-const { handleFormData, uploadImg } = require('../_image-and-video-storage');
-const { tempStorageTruncate } = require('../_helpers');
+const { handleFormData, deleteImg } = require('../_image-and-video-storage');
+const { uploadOrUpdateImg } = require('../_helpers');
 
 module.exports = router;
 
-router.post('/image', handleFormData.single('img'), async (req, res) => {
-  const { folderName, description = null } = req.body;
+router.post('/image/upload', handleFormData.single('img'), uploadOrUpdateImg);
 
-  //   Send back error when image is not sent right
+router.put('/image/update', handleFormData.single('img'), uploadOrUpdateImg);
+
+router.delete('/image/delete', handleFormData.none(), async (req, res) => {
+  const {
+    body: { folderName, imgId }
+  } = req;
+
   if (!folderName) {
-    return res.status(500).send({
-      Error_message:
-        'Missing destination folder in form, matching key "folderName"'
-    });
+    return res
+      .status(400)
+      .send({ Error_message: 'Missing parameter folderName' });
   }
-  // Check description type to be of type string
-  if (description !== null && typeof description !== 'string') {
-    return res.status(500).send({
-      Error_message: 'image description must be of type string'
-    });
-  }
-  //  Check if file is included in request
-  if (req.file.path === null || undefined) {
-    return res.status(500).send({
-      Error_message:
-        'No image found in request! You have to send a image appended to a form to this route.'
-    });
+
+  if (!imgId) {
+    return res.status(400).send({ Error_message: 'Missing parameter imgId' });
   }
 
   try {
-    /*
-    Extract img form req object through req.file path and send to 
-    Cloudinary upload function. Second parameter is foldername on Cloudinary 
-    */
+    const cloudinaryResult = await deleteImg(imgId);
 
-    const cloudinaryResult = await uploadImg(req.file.path, folderName);
+    if (cloudinaryResult === 'not found') {
+      return res.status.send({ Error_message: cloudinaryResult });
+    }
 
-    console.log(cloudinaryResult);
-
-    /* 
-    Result form upload is an object where public id and https link is saved and sent back in response
-    */
-
-    const { public_id, secure_url } = await cloudinaryResult;
-
-    res.send({
-      message: `image succefully saved on cloudinary in folder ${folderName}`,
-      'img-link': secure_url,
-      'public-id': public_id
-    });
-  } catch (err) {
-    /* If error send error response with catched error as feedback */
-    console.error(err);
-    res.status(500).send({ Error_message: err });
+    return res.send({ message: cloudinaryResult });
+  } catch (error) {
+    res.status(500).send({ Error_message: error });
   }
-
-  tempStorageTruncate();
 });
