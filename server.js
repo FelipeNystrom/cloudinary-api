3650e5b5794dc21a38a145bfbbe73f7b88191208
FelipeNystrom <andre.felipe.nystrom@gmail.com>
2019-02-21 06:25:24 +0100

    remove cors and other not used packages to align to microservice architechture

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const server = express();
 const mountRoutes = require('./_routes');
-const { corsOptions } = require('./_helpers');
-const cors = require('cors');
 const port = process.env.PORT || 6000;
 
 // server configs
 server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
-server.use(cors({ corsOptions }));
-// Enables pre-flight request to put, del, option requests
-server.options('*', cors());
 
 mountRoutes(server);
 
